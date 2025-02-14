5ccd90f283a3cee5fca07517888b9bb51a4b2efe
lmejiamartinez <lmejiamartinez25@gmail.com>
2025-02-13 20:31:13 -0500

    Cambion en Controlador aprendiz (Obtener,Eliminar)

--- backend/Server/Controllers/aprendicesController.js
+++ backend/Server/Controllers/aprendicesController.js
@@ -25,6 +25,30 @@ exports.Obteneraprendices = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+//Obtener aprendices sacando a los inactivos
+exports.Obteneraprendices = async (req, res) =>{
+    try{
+        let{page, size} = req.query;
+        page = page ? parseInt(page): 1;
+        sizw = sixze? parseInt(size):10;
+        const offset = (page - 1) * size;
+        const limit = size;
+
+        const aprendices = await Aprendiz.findAndCountAll({
+            where:{activo: true},//Para filtrar a los activos
+            limit,
+            offset
+        });
+        res.status(200).json({
+            total:aprendices.count,
+            totalPages:Math.ceil(aprendixces.count / size),
+            data:aprendices.rows
+        });
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
 //Obtener aprendices con ID
 exports.ObteneraprendicesID = async (req, res) => {
     try {
@@ -52,17 +76,35 @@ exports.ActualizaraprendizID = async(req, res) => {
         res.status(500).json({error:error.message})
     }
 
-}
-
-//Eliminar Aprendiz ID
-exports.EliminaraprendizID = async(req,res) =>{
-    try{
-        const filasEliminadas = await Aprendiz.destroy
-        ({where: {id: req.param.id}});
-        if(!filasEliminada)
-        return res.status(404).json ({error: 'Aprendiz no encontrado'});
-    }catch(error){
-        return res.status(500).json ({error: error.message})
-    }
-
 };
+
+// Eliminar Aprendiz por ID
+exports.EliminaraprendizID = async (req, res) => {
+    try {
+        const filasEliminadas = await Aprendiz.destroy({
+            where: { id: req.params.id }
+        });
+
+        if (!filasEliminadas) {
+            return res.status(404).json({ error: 'Aprendiz no encontrado' });
+        }
+
+        return res.json({ mensaje: "Aprendiz eliminado correctamente" });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+};
+//Elimnar  desactivar aprendiz
+exports.EliminaraprendizID = async (req, res) => {
+    try{
+        const aprendiz = await Aprendiz.findByPk(req.params.id);
+        if(!aprendiz)
+        return res.status(404).json({error: 'Aprendiz no encontrado'});
+        aprendiz.actico = false;
+        await aprendiz.save();
+
+        res.status(200).json({message: 'Aprendiz marcado como inactivo'});
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+}
