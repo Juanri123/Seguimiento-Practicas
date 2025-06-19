22dc4847d93c15e72e6469bd99a5afd55790279b
yefferson <londonojefferson04@gmail.com>
2025-06-19 08:35:14 -0500

    Mejora en rendimiento y carga de archivos integrando Lazy

--- src/components/inicio/Routing.jsx
+++ src/components/inicio/Routing.jsx
@@ -1,42 +1,45 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { useLocalStorage } from "react-use";
-import Login from "../login/Login";
-import Register from "../login/Register";
-import Bitacoras from "../bitacoras/MainContent";
-import Inicio from "../inicio/Inicio";
-import Usuarios from "../usuarios/Usuarios";
-import Visitas from "../visitas/Visitas";
-import Reportes from "../reportes/Reportes";
-import Fichas from "../ficha/fichas";
-import ForgotPassword from "../login/ForgotPassword";
-import ResetPassword from "../login/ResetPassword";
-import NotFound from "./NotFound";
-import Content from "../generales/Content";
-import RutasProtegidas from "../utils/RutasProtegidas";
-import AjustesUsuario from "../usuarios/AjustesUsuario";
+import { lazy, Suspense } from "react";
+const Login = lazy(() => import("../login/Login"));
+const Register = lazy(() => import("../login/Register"));
+const Bitacoras = lazy(() => import("../bitacoras/MainContent"));
+const Inicio = lazy(() => import("../inicio/Inicio"));
+const Usuarios = lazy(() => import("../usuarios/Usuarios"));
+const Visitas = lazy(() => import("../visitas/Visitas"));
+const Reportes = lazy(() => import("../reportes/Reportes"));
+const Fichas = lazy(() => import("../ficha/fichas"));
+const ForgotPassword = lazy(() => import("../login/ForgotPassword"));
+const ResetPassword = lazy(() => import("../login/ResetPassword"));
+const NotFound = lazy(() => import("../inicio/NotFound"));
+const Content = lazy(() => import("../generales/Content"));
+const RutasProtegidas = lazy(() => import("../utils/RutasProtegidas"));
+const AjustesUsuario = lazy(() => import("../usuarios/AjustesUsuario"));
 
 function Routing() {
   const [user] = useLocalStorage("usuario");
 
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/registro" element={<Register />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route element={<RutasProtegidas acceso={user} />}>
-          <Route path="/inicio" element={<Inicio />} />
-          <Route path="/usuarios" element={<Usuarios />} />
-          <Route path="/usuarios/id" element={<Content />} />
-          <Route path="/visitas" element={<Visitas />} />
-          <Route path="/fichas" element={<Fichas />} />
-          <Route path="/bitacoras" element={<Bitacoras />} />
-          <Route path="/reportes" element={<Reportes />} />
-          <Route path="/ajustes" element={<AjustesUsuario />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<div>Cargando página ...</div>}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/registro" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route element={<RutasProtegidas acceso={user} />}>
+            <Route path="/inicio" element={<Inicio />} />
+            <Route path="/usuarios" element={<Usuarios />} />
+            <Route path="/usuarios/id" element={<Content />} />
+            <Route path="/visitas" element={<Visitas />} />
+            <Route path="/fichas" element={<Fichas />} />
+            <Route path="/bitacoras" element={<Bitacoras />} />
+            <Route path="/reportes" element={<Reportes />} />
+            <Route path="/ajustes" element={<AjustesUsuario />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 }
