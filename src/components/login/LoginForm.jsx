8bc80da6e2b4c99eae50ec0904f29b81c1ea78c2
yefferson <londonojefferson04@gmail.com>
2025-06-19 11:58:50 -0500

    Organización de Carpeta y archivo del globalVars

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -2,7 +2,7 @@
 	import { useState, useEffect } from 'react'
 	import { Link, useNavigate } from 'react-router-dom'
 	import Swal from 'sweetalert2'
-	import { API_URL } from '../../api/globalVars'
+	import { API_URL } from '../../../api/globalVars'
 
 	const LoginForm = () => {
 		const [formData, setFormData] = useState({
